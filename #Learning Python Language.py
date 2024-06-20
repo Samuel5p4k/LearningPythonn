6dd8cd63c77c2fb7d88425016412fa95e9ca1934
Samuel5p4k <39774905+Samuel5p4k@users.noreply.github.com>
2024-06-20 18:44:36 +0300

    first commit

---
+++ #Learning Python Language.py
@@ -0,0 +1,18 @@
+#Learning Python Language Pythin in a nutshell book
+print("Hello, World!")
+
+#comments
+'''Multiple line comments are
+ comments that span multiple line of code
+they are written by enclosing the comment text in triple 
+single or double quotation marks.'''
+
+"""exhibit 
+    two """
+
+#Variables
+
+a = "Samuel"
+b = "Kikule"
+c = "CSC Student"
+print(a, b,"is a good", c)
